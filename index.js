44a806bf3f7722b2dfb9b3bbb01d827d8be2612e
Álvaro Reig González <alvaroreig@gmail.com>
2016-09-02 19:38:52 +0200

    working send email

--- index.js
+++ index.js
@@ -3,17 +3,36 @@ var request = require('request');
 var exec = require('child_process').exec;
 
 var create_ebook=true;
+var send_ebook_to_kindle=true;
 var archive_in_pocket=false;
 
 if (create_ebook){
 	var create_ebook_command = 'ebook-convert ' + process.env.CALIBRE_POCKETPLUS_RECIPE + 
 		' ' + process.env.CALIBRE_OUTPUT_FILE + ' --username ' + process.env.POCKET_USERNAME + 
 		' --password ' + process.env.POCKET_PASSWORD;
-
+	
+	//var create_ebook_command = 'ls';
 
 	exec(create_ebook_command, function(error, stdout, stderr) {
 		console.log(stdout);
 		console.log(stderr);
+
+		if (send_ebook_to_kindle){
+			var send_ebook_command='calibre-smtp --attachment ' + process.env.CALIBRE_OUTPUT_FILE
+				+ ' --relay ' + process.env.SMTP_SERVER + ' --port ' + process.env.SMTP_PORT
+				+ ' --username ' + process.env.SMTP_USERNAME + ' --password '
+				+ process.env.SMTP_PASSWORD + ' --encryption-method ' + process.env.SMTP_ENCRYPT
+				+ ' --subject PocketToKindle ' + process.env.SMTP_FROM
+				+ ' ' + process.env.KINDLE_ADDRESS + ' PocketToKindle';
+
+			console.log(send_ebook_command);
+
+			exec(send_ebook_command, function(error, stdout, stderr) {
+				console.log(stdout);
+				console.log(stderr);
+			});
+
+		}
 	});
 }
 
