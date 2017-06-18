2b992bf864a45d97cf91e20b8bea8eced4de4790
primolarry <alvaroreig@gmail.com>
2017-06-18 08:59:18 +0000

    testing ebook creation

--- test/test.js
+++ test/test.js
@@ -1,7 +1,13 @@
 var assert = require("assert"); // node.js core module
 require('dotenv').config({silent: true});
 const winston = require('winston');
+winston.level = process.env.TEST_LOG_LEVEL;
+
 var mailgun_helper = require('../src/mailgun_helper');
+var calibre_helper = require('../src/calibre_helper');
+
+
+
 
 describe('mailgun_helper.js', function(){
   it('should send an email using sendEmail(data,callback()', function(done){
@@ -13,8 +19,19 @@ describe('mailgun_helper.js', function(){
     	text: 'Email body while testing with mocha'
   	};
   	
-    mailgun_helper.sendEmail(data,function(){
+    mailgun_helper.sendEmail(data,function(status){
+        assert.equal(status,0);
         done();
     });
+  })
+});
+
+describe('calibre_helper.js', function(){
+  it('should create an ebbok using createEbookWithPocketContent(callback()', function(done){
+    this.timeout(10000);
+    calibre_helper.createEbookWithPocketContent(function(status){
+		  assert.equal(status,0);
+      done();
+		});
   })
 });
